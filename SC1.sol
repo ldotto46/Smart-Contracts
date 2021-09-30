2907a743a3eefdd767ba9092d132e541f3b07a23
Luis Dotto <76250515+ldotto46@users.noreply.github.com>
2021-09-29 23:31:18 -0300

    Update SC1.sol

--- SC1.sol
+++ SC1.sol
@@ -1,4 +1,3 @@
-
 pragma solidity ^0.6.0;
 
 //define contract !Remember every contract cost GAS
@@ -6,18 +5,28 @@ contract Storage {
 
     // this will get initialized to 0!
     uint256 public favoriteNumber;
+    bool favoriteBool;
+    bool favoriteBool2;
 
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    
+    // ARRAY similar to Python. List or a group of some objects. 
+    
+    People public person = People({favoriteNumber: 2, name: "Luigi"});
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
-        uint256 test = 4;
-        
     }    
     
-    // view means that I want to read some state of the blockchain. !!!!BLue bottons = VIEW!!!! 
-    // pure 
+    // view means that I want to read some state of the blockchain. !!!!BLUE bottons = VIEWS!!!! 
+    // pure Math just for algebra
     function retrieve() public view returns(uint256) {
         return favoriteNumber;
     }
     
 }
+
+// STRUCT ways to define objects like "People"
