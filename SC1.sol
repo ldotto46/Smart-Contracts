c7bb8030ba13fd5b909af543ad78248f146181e6
Luis Dotto <76250515+ldotto46@users.noreply.github.com>
2021-10-04 21:14:58 -0300

    details zombies

--- SC1.sol
+++ SC1.sol
@@ -2,45 +2,7 @@
 
 pragma solidity ^0.6.0;
 
-//define contract !Remember every contract cost GAS
-contract Storage {
-
-    // this will get initialized to 0!
-    uint256 public favoriteNumber;
-    bool favoriteBool;
-
-    struct People {
-        uint256 favoriteNumber;
-        string name;
-    }
-    
-    // ARRAY similar to Python. List or a group of some objects. FUnciona igual que lo anterior. SI pongo entre los corchetes 1 digo que solo tiene maximo 1
-    
-    People[1] public people;
-    
-    People public person = People({favoriteNumber: 2, name: "Luigi"});
-
-    function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
-    }    
-    
-    // view means that I want to read some state of the blockchain. !!!!BLUE bottons = VIEWS!!!! 
-    // pure Math just for algebra
-    function retrieve() public view returns(uint256) {
-        return favoriteNumber;
-    }
-// Con memory guardamos data durante la ejecucion de la FUncion
-// Con storage guardamos la data y queda guardada
-
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        people.push(People(_favoriteNumber, _name));
-    }
-}
-
-// STRUCT ways to define objects like "People"
-
-
-// FIGHT BETWEEN PEOPLE AND ZOMBIES
+// 1. CREATING ZOMBIE FACTORY
 
 contract ZombieFactory {
 
